9ed8b64e4b5e82c04f1275a26e1d4ad849eb5643
gam3boy1 <aadianand1234@gmail.com>
2024-11-27 17:53:22 -0500

    minor updates to files

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './views/App.jsx'
-import { BrowserRouter } from "react-router-dom"
-import "jquery"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./views/App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "jquery";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,41 +1,53 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 // import reactLogo freact
-import PostView from './PostView'
+import PostView from "./PostView";
 import { Routes, Route } from "react-router-dom";
-import NavBar from './NavBar';
-import CategoryPostsView from './CategoryView';
-import "../stylesheets/App.css"
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import NavBar from "./NavBar";
+import CategoryPostsView from "./CategoryView";
+import "../stylesheets/App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 export default function App() {
-    const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
-    function imgSrc() {
-        return darkMode ? "https://cdn-icons-png.flaticon.com/512/180/180700.png" : "https://cdn-icons-png.flaticon.com/512/1779/1779906.png"
-    }
+  function imgSrc() {
+    return darkMode
+      ? "https://cdn-icons-png.flaticon.com/512/180/180700.png"
+      : "https://cdn-icons-png.flaticon.com/512/1779/1779906.png";
+  }
 
-    return (
-        <>
-            <div className="blog-body">
-                <NavBar/>
-                <div className="blog-content">
-                    <Routes>
-                        <Route exact path="/" element={<CategoryPostsView home={true}/>}/>
-                        <Route exact path="/category/:categoryID" element={<CategoryPostsView home={false}/>}/>
-                        <Route exact path="/posts/:postID" element={<PostView/>}/>
-                    </Routes>
-                </div>
-                <footer>
-                    <Navbar bg="body-secondary">
-                        <Container fluid>
-                            <Nav className="me-auto">
-                                <Nav.Link href="/">Copyright © 2024 Aadi Anand</Nav.Link>
-                            </Nav>
-                            <img className="d-flex mx-3" src={imgSrc()} width="30px" role="button" onClick={() => setDarkMode(!darkMode)}/>
-                        </Container>
-                    </Navbar>
-                </footer>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="blog-body">
+        <NavBar />
+        <div className="blog-content">
+          <Routes>
+            <Route exact path="/" element={<CategoryPostsView home={true} />} />
+            <Route
+              exact
+              path="/category/:categoryID"
+              element={<CategoryPostsView home={false} />}
+            />
+            <Route exact path="/posts/:postID" element={<PostView />} />
+          </Routes>
+        </div>
+        <footer>
+          <Navbar bg="body-secondary">
+            <Container fluid>
+              <Nav className="me-auto">
+                <Nav.Link href="/">Copyright © 2024 Aadi Anand</Nav.Link>
+              </Nav>
+              <img
+                className="d-flex mx-3"
+                src={imgSrc()}
+                width="30px"
+                role="button"
+                onClick={() => setDarkMode(!darkMode)}
+              />
+            </Container>
+          </Navbar>
+        </footer>
+      </div>
+    </>
+  );
 }
