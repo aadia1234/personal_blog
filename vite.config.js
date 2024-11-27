d9116ae918e7a3b942bba331cf17cc85683a7a07
gam3boy1 <aadianand1234@gmail.com>
2024-11-27 18:01:09 -0500

    adde predeploy and deploy scripts for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/aadia1234.github.io",
   server: {
     port: 5173,
-    hot: true
-  }
-})
+    hot: true,
+  },
+});
