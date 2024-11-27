4afb68c390769279bbec885664917176ff7d257c
gam3boy1 <aadianand1234@gmail.com>
2024-11-27 18:05:57 -0500

    updated base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/aadia1234.github.io",
   server: {
     port: 5173,
     hot: true,
